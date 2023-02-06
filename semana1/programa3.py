e2eeae6d2906462e4f72f5b6803ff52a910552f9
JairVaz13 <122801418+JairVaz13@users.noreply.github.com>
2023-02-06 00:01:49 +0000

    Sacar cálculadora

---
+++ semana1/programa3.py
@@ -0,0 +1,12 @@
+numero1 = input("numero 1: ")
+numenumero1ro2 = 10
+potencia = int(numero1) ** numero2
+divicion = int(numero1) / numero2
+multiplicacion = int(numero1) * numero2
+resta = int(numero1) - numero2
+suma = int(numero1) + numero2
+print(potencia)
+print(divicion)
+print(multiplicacion)
+print(resta)
+print(suma)
